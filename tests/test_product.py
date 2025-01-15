bcbbbf4d13978b232e6e2ca67b1d869a0d8658ba
SZilean <adil.bouzit@gmail.com>
2025-01-15 14:16:32 +0100

    add:test: création de fichier de test de la fonctionnalité get_item_count

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,4 +1,11 @@
+import sys
+import os
 import unittest
+
+# Ajoute le répertoire parent au chemin de recherche des modules
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from cart import Cart
 from product import Product
 
 class TestProduct(unittest.TestCase):
