064d496f341c878c9f0fbfcb1cac517364051755
SZilean <adil.bouzit@gmail.com>
2025-01-16 18:40:51 +0100

    cache

--- tests/test_order.py
+++ tests/test_order.py
@@ -21,8 +21,6 @@ class TestOrder(unittest.TestCase):
 
         self.cart.add_product(self.p1, 2)  #(2 * 1000 = 2000€)
         self.cart.add_product(self.p2, 4)  #(4 * 50 = 200€)
-        self.cart.add_product(self.p3, 5)
-        self.cart.mettre_de_cote(self.p3)
 
         self.order = Order(self.cart) # 2200€
         
@@ -35,6 +33,7 @@ class TestOrder(unittest.TestCase):
         total = 2200 - (2200 * 0.1)
         self.assertEqual(self.order.total, total)
         print(f'{self.order.total}')
+        self.test_generate_receipt()
         print("[Test] Discount order passed.")
     
     def test_generate_receipt(self):
@@ -44,8 +43,8 @@ class TestOrder(unittest.TestCase):
 
     def test_cancel_order(self):
         print("[Test] Testing cancel_order...")
-        self.order.cancel_order()
-        self.assertEqual(self.p1.stock, 5)
+        self.order.cancel_order()        
+        self.assertEqual(self.p1.stock, 7)
         print("[Test] cancel_order passed.")
 
 if __name__ == "__main__":
