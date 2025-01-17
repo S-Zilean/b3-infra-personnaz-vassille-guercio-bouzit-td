1d24804210d1d197bbf00736948a806b03fa1031
SZilean <adil.bouzit@gmail.com>
2025-01-17 16:23:59 +0100

    comm: rajout des créateurs de fonction en commentaire

--- tests/test_order.py
+++ tests/test_order.py
@@ -16,16 +16,13 @@ class TestOrder(unittest.TestCase):
         self.p1 = Product("Laptop", 1000, 5)
         self.p2 = Product("Headphones", 50, 20)
         self.p3 = Product("Mouse", 25, 10)
-
         self.cart = Cart()
-
         self.cart.add_product(self.p1, 2)  #(2 * 1000 = 2000€)
         self.cart.add_product(self.p2, 4)  #(4 * 50 = 200€)
-
-        self.order = Order(self.cart) # 2200€
-        
+        self.order = Order(self.cart) # 2200€       
         print("\n[Setup] Created a Cart instance for testing.")
 
+    #Fonction de test de Pablo
     def test_discount_code(self):
         print("[Test] Discount order...")
         self.order.discount_code("SOLDE10")
@@ -33,22 +30,28 @@ class TestOrder(unittest.TestCase):
         self.assertEqual(self.order.total, total)
         print("[Test] Discount order passed.")
     
+    #Fonction de test d'Adil
     def test_generate_receipt(self):
         print("[Test] Testing generate_receipt...")
         self.order.generate_receipt()
         print("[Test] Get generate_receipt passed.")
-        
+
+    #Fonction de test de Jeremy
+    def test_cancel_order(self):
+        print("[Test] Testing cancel_order...")
+        self.order.cancel_order()        
+        self.assertEqual(self.p1.stock, 7)
+        print("[Test] cancel_order passed.")
+
+
+    #Fonctions de test Nicolas 
+     
     def test_update_order(self):
         self.order.update_order(self.laptop, 1)
         self.assertEqual(self.order.items[self.laptop], 1)
         expected_total = sum(p.price * q for p, q in self.order.items.items())
         self.assertAlmostEqual(self.order.total, expected_total)
 
-    def test_cancel_order(self):
-        print("[Test] Testing cancel_order...")
-        self.order.cancel_order()        
-        self.assertEqual(self.p1.stock, 7)
-        print("[Test] cancel_order passed.")
 
     def test_update_order_invalid_product(self):
         fake_product = Product(name="Fake", price=100.0, stock=1)
